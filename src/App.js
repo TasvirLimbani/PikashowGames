f65308e5f4e89035d81ad69c27ba52e5b09a70a1
TasvirLimbani <tasvirlimbani111@gmail.com>
2025-11-08 19:10:13 +0530

    close inspect elements
    close inspect elements

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./Components/Home";
-import Detailpage from "./Components/Detailpage";
-import About from "./Components/AboutUs";
-import { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Components/Home';
+import Detailpage from './Components/Detailpage';
+import About from './Components/AboutUs';
+import { useEffect } from 'react';
 
 function App() {
-  // useEffect(() => {
-    // const handleContextMenu = (e) => e.preventDefault();
-    // const handleKeyDown = (e) => {
-    //   if (
-    //     e.key === "F12" ||
-    //     (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
-    //     (e.ctrlKey && e.key === "U")
-    //   ) {
-    //     e.preventDefault();
-    //   }
-    // };
+  useEffect(() => {
+    const handleContextMenu = (e) => e.preventDefault();
+    const handleKeyDown = (e) => {
+      if (
+        e.key === "F12" ||
+        (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
+        (e.ctrlKey && e.key === "U")
+      ) {
+        e.preventDefault();
+      }
+    };
 
-    // document.addEventListener("contextmenu", handleContextMenu);
-    // document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("contextmenu", handleContextMenu);
+    document.addEventListener("keydown", handleKeyDown);
 
-  //   return () => {
-  //     document.removeEventListener("contextmenu", handleContextMenu);
-  //     document.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // }, []);
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={ <Home /> } />
           <Route path="/details/:id" element={<Detailpage />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
+
     </>
   );
 }
